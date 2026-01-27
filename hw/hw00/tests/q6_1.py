bd9594595eb75b930d5e3b9ad5e86a095bb36721
Elizabeth <elizabeth.spaulding@colorado.edu>
2026-01-27 12:22:58 -0600

    force save

--- hw/hw00/tests/q6_1.py
+++ hw/hw00/tests/q6_1.py
@@ -1,3 +1,3 @@
 OK_FORMAT = True
 
-test = {'name': 'q6_1', 'points': 5, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+test = {'name': 'q6_1', 'points': [0], 'suites': [{'cases': [{'code': '>>> len(survey) == 5\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
