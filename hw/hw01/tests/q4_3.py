4c09b74c16c67f60546ecfdac890249679aef5c8
Elizabeth <elizabeth.spaulding@colorado.edu>
2026-01-27 10:18:48 -0600

    add hw01

---
+++ hw/hw01/tests/q4_3.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q4_3',
+    'points': [0, 0, 0],
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(phil_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> isinstance(soc_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> isinstance(eng_relative_change, (int, float))\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
